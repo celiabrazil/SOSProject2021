880de11ee7c78e71501ac96426e0b45ed2716cb5
aashakya <67236701+aashakya@users.noreply.github.com>
2021-07-23 22:33:23 -0500

    Code clean-up + few edit

--- src/components/NewSale.jsx
+++ src/components/NewSale.jsx
@@ -27,130 +27,126 @@ const theme = {
 <Grommet theme={theme}></Grommet>;
 
 function NewSale() {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
   return (
-    <Grommet plain>
-        <Grid //top half
+    <Grommet plain className="m-5 p-5">
+      <Grid //top half
+        columns={{
+          count: 3,
+          size: "auto",
+        }}
+        gap="large"
+      >
+        <Box pad="medium">
+          <FileInput //Upload Images
+            multiple
+            onChange={(event, { files }) => {
+              console.log(event);
+              for (let i = 0; i < files.length; i += 1) {
+                const file = files[i];
+                console.log(file.name);
+              }
+            }}
+          />
+        </Box>
+
+        <Box width="large" pad="medium">
+          <Grid
             columns={{
-                count: 3,
-                size: 'auto',
+              count: 3,
+              size: "auto",
             }}
             gap="large"
-        >
-            <Box pad="medium">
-                <FileInput //Upload Images
-                    multiple
-                    onChange={(event, { files }) => {
-                        console.log(event);
-                        for (let i = 0; i < files.length; i += 1) {
-                        const file = files[i];
-                        console.log(file.name);
-                        }
-                    }}
-                    />              
-            </Box>
-
-            <Box
-                width="large"
-                pad="medium"
-            >
-                <Grid
-                    columns={{
-                        count: 3,
-                        size: 'auto',
-                    }}
-                    gap="large"
-                    >
-                
-                    <Box>
-                        <Box>
-                            <TextInput //Manufacturer
-                                    placeholder="Manufacturer"
-                                    size="large"
-                                    value={value}
-                                    onChange={event => setValue(event.target.value)} 
-                                />
-                        </Box>
-                            <Box background="000000" height="20px">
-                                <Text>  </Text>
-                            </Box>
-                        <Box>
-                            <TextInput //Car Name
-                                    placeholder="Car Name"
-                                    size="large"
-                                    value={value}
-                                    onChange={event => setValue(event.target.value)} 
-                                />
-                        </Box> 
-                    </Box>                  
-                    <Box>
-                        <Box>
-                            <TextInput //Price
-                                    placeholder="Price"
-                                    size="large"
-                                    value={value}
-                                    onChange={event => setValue(event.target.value)} 
-                                />
-                        </Box>
-                        <Box background="000000" height="20px">
-                                <Text>  </Text>
-                            </Box>
-                        <Box>
-                            <TextInput //Battery Percentage
-                                    placeholder="Battery Percentage"
-                                    size="large"
-                                    value={value}
-                                    onChange={event => setValue(event.target.value)} 
-                                />
-                        </Box>                        
-                    </Box>
-                    <Box>
-                        <Box>
-                            <TextInput //Car Mileage
-                                placeholder="Car Mileage"
-                                size="large"
-                                value={value}
-                                onChange={event => setValue(event.target.value)} 
-                            />
-                        </Box>
-                        <Box background="000000" height="20px">
-                                <Text>  </Text>
-                            </Box>
-                        <Box>
-                            <TextInput //Date Bought
-                                    placeholder="Date Bought"
-                                    size="large"
-                                    value={value}
-                                    onChange={event => setValue(event.target.value)} 
-                                />
-                        </Box>
-                    </Box>
-                </Grid>
-            </Box>
-
-
+          >
             <Box>
-                <Button 
-                    primary label="Save" 
-                    //href="./review"
-                    alignSelf="center"
-                    size="xxlarge"
-                    margin="medium"
-                    color="#006400"/>
+              <Box>
+                <TextInput //Manufacturer
+                  placeholder="Manufacturer"
+                  size="large"
+                  value={value}
+                  onChange={(event) => setValue(event.target.value)}
+                />
+              </Box>
+              <Box background="000000" height="20px">
+                <Text> </Text>
+              </Box>
+              <Box>
+                <TextInput //Car Name
+                  placeholder="Car Name"
+                  size="large"
+                  value={value}
+                  onChange={(event) => setValue(event.target.value)}
+                />
+              </Box>
             </Box>
+            <Box>
+              <Box>
+                <TextInput //Price
+                  placeholder="Price"
+                  size="large"
+                  value={value}
+                  onChange={(event) => setValue(event.target.value)}
+                />
+              </Box>
+              <Box background="000000" height="20px">
+                <Text> </Text>
+              </Box>
+              <Box>
+                <TextInput //Battery Percentage
+                  placeholder="Battery Percentage"
+                  size="large"
+                  value={value}
+                  onChange={(event) => setValue(event.target.value)}
+                />
+              </Box>
+            </Box>
+            <Box>
+              <Box>
+                <TextInput //Car Mileage
+                  placeholder="Car Mileage"
+                  size="large"
+                  value={value}
+                  onChange={(event) => setValue(event.target.value)}
+                />
+              </Box>
+              <Box background="000000" height="20px">
+                <Text> </Text>
+              </Box>
+              <Box>
+                <TextInput //Date Bought
+                  placeholder="Date Bought"
+                  size="large"
+                  value={value}
+                  onChange={(event) => setValue(event.target.value)}
+                />
+              </Box>
+            </Box>
+          </Grid>
+        </Box>
 
-        </Grid>
-            <Box margin="large" size="medium">
-                <TextArea //Description
-                    placeholder="Description"
-                    size="large"
-                    value={value}
-                    onChange={event => setValue(event.target.value)} 
-                    />
-            </Box>
+        <Box>
+          <Button
+            primary
+            label="Save"
+            //href="./review"
+            alignSelf="center"
+            size="xxlarge"
+            margin="medium"
+            color="#006400"
+            onClick={console.log("Wew")}
+          />
+        </Box>
+      </Grid>
+      <Box margin="large" size="medium">
+        <TextArea //Description
+          placeholder="Description"
+          size="large"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+      </Box>
     </Grommet>
   );
 }
 
 export default NewSale;
-
--- src/components/mapComponents/Map.jsx
+++ src/components/mapComponents/Map.jsx
@@ -1,70 +1,69 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import './map.css';
-import {Icon,InlineIcon} from '@iconify/react';
-import outlinePlace from '@iconify-icons/ic/outline-place';
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import "./map.css";
+import { Icon } from "@iconify/react";
+import outlinePlace from "@iconify-icons/ic/outline-place";
 let locations = [
-    {
-        name: `Tesla Indianapolis`,
-        location:{lat:39.906990,lng:-86.071300},
-        address:'8280 Castleton Corner Dr,\n Indianapolis, IN\n 46250'
-    },
-    {
-        name: `Hubler Chevrolet, Inc`,
-        location:{lat:39.645600,lng:-86.134200},
-        address:'8220 US-31,\n Indianapolis, IN\n 46227'
-    },
-    {
-        name: `O'brien Toyota`,
-        location:{lat:39.804190,lng:-86.048140},
-        address:'2550 Shadeland Ave,\n Indianapolis, IN\n 46219'
-    }
+  {
+    name: `Tesla Indianapolis`,
+    location: { lat: 39.90699, lng: -86.0713 },
+    address: "8280 Castleton Corner Dr,\n Indianapolis, IN\n 46250",
+  },
+  {
+    name: `Hubler Chevrolet, Inc`,
+    location: { lat: 39.6456, lng: -86.1342 },
+    address: "8220 US-31,\n Indianapolis, IN\n 46227",
+  },
+  {
+    name: `O'brien Toyota`,
+    location: { lat: 39.80419, lng: -86.04814 },
+    address: "2550 Shadeland Ave,\n Indianapolis, IN\n 46219",
+  },
 ];
 
-
-
 const LocationPin = ({ title, address }) => (
-    <div className="pin">
-      <Icon icon={outlinePlace} className="location-icon" />
-      <div className="location-text">
-        <h3 className="location-title">{title}</h3>
-        <p className="location-address">{address}</p>
-      </div>
+  <div className="pin">
+    <Icon icon={outlinePlace} className="location-icon" />
+    <div className="location-text">
+      <h3 className="location-title">{title}</h3>
+      <p className="location-address">{address}</p>
     </div>
-  );
-
+  </div>
+);
 
 const Map = ({ location, zoomLevel }) => (
-    <div className="map">
-      <h2 className="map-h2">Closest Dealerships</h2>
-  
-      <div className="google-map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAn8iO-ytPwkhCxQISqiKnHGhznyowI90A' }}
-          defaultCenter={location}
-          defaultZoom={zoomLevel}
-        >
-          <LocationPin
-            lat={locations[0].location.lat}
-            lng={locations[0].location.lng}
-            title={locations[0].name}
-            address={locations[0].address}
-            />
-            <LocationPin
-            lat={locations[1].location.lat}
-            lng={locations[1].location.lng}
-            title={locations[1].name}
-            address={locations[1].address}
-            />
-            <LocationPin
-            lat={locations[2].location.lat}
-            lng={locations[2].location.lng}
-            title={locations[2].name}
-            address={locations[2].address}
-            />
-        </GoogleMapReact>
-      </div>
-    </div>
-  );
+  <div className="map">
+    <br />
+    <h2 className="map-h2">Nearest Dealerships</h2>
+    <br />
 
-export default Map
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAn8iO-ytPwkhCxQISqiKnHGhznyowI90A" }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin
+          lat={locations[0].location.lat}
+          lng={locations[0].location.lng}
+          title={locations[0].name}
+          address={locations[0].address}
+        />
+        <LocationPin
+          lat={locations[1].location.lat}
+          lng={locations[1].location.lng}
+          title={locations[1].name}
+          address={locations[1].address}
+        />
+        <LocationPin
+          lat={locations[2].location.lat}
+          lng={locations[2].location.lng}
+          title={locations[2].name}
+          address={locations[2].address}
+        />
+      </GoogleMapReact>
+    </div>
+  </div>
+);
+
+export default Map;
